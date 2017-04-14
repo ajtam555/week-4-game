8f67f8339663776e66de494cd5bac13a8061a678
Alex Tam <AJT@Alexanders-MacBook-Pro.local>
2017-04-13 23:39:49 -0700

    some initializer code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,20 @@
- console.log("something");
+// Variables
+
+// Function
+function initialize() {
+	var targetNum = Math.floor(Math.random() * 120 + 19);
+	var carNum = Math.floor(Math.random() * 19 + 1);
+}
+
+// Cars Game
+$(document).ready(function() {
+	initialize();
+
+});
+
+
+
+
+
+
+
